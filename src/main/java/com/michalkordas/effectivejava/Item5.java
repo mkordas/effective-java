0ff6e1e9ca162b033c1578792ba887d870e766dc
Michal Kordas <kordas.michal@gmail.com>
2016-08-15 22:43:48 +0200

    Add code for item 5

---
+++ src/main/java/com/michalkordas/effectivejava/Item5.java
@@ -0,0 +1,13 @@
+package com.michalkordas.effectivejava;
+
+class Item5 {
+
+    // What is so slow here?
+    public static void main(String[] args) {
+        Long sum = 0L;
+        for (long i = 0; i < Integer.MAX_VALUE; i++) {
+            sum += i;
+        }
+        System.out.println(sum);
+    }
+}

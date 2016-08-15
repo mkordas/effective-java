7c82559c51ef48caeb2732912935c3cb8ca24201
Michal Kordas <kordas.michal@gmail.com>
2016-08-15 22:49:35 +0200

    Add code for item 8

---
+++ src/main/java/com/michalkordas/effectivejava/Item8.java
@@ -0,0 +1,32 @@
+package com.michalkordas.effectivejava;
+
+public class Item8 {
+
+    public static void main(String[] args) {
+        new Point(1, 1);
+    }
+}
+
+class Point {
+
+    private final int x;
+    private final int y;
+
+    Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+}
+
+class ColorPoint {
+
+    private final int x;
+    private final int y;
+    private final String color;
+
+    ColorPoint(int x, int y, String color) {
+        this.x = x;
+        this.y = y;
+        this.color = color;
+    }
+}
